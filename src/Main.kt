b113c4379fbade85af533e577c35bde1051c6242
HyeonseungSon <boywin8636@gmail.com>
2021-07-22 13:53:41 +0900

    Function Programming (Inflearn, No.36) Date / Time : 2021.07.22 13:53 Location : Man_Deum Description : Inflearn Kotlin Study

---
+++ src/Main.kt
@@ -0,0 +1,11 @@
+/*
+Function Programming
+ - Java 언어는 완벽한 객체지향 Programing 언어입니다.
+ - 모든 Code 는 Class 를 설계하고 Method 를 만들어주고
+ Class 를 통해 객체를 생성해서 사용해야한다.
+ - 하지만 Kotlin 은 함수만 만들어 사용하는 것을 지원한다.
+ - Kotlin 은 함수 사용을 보다 편리하게 할 수 있도록 하는 다양한 개념들이 있다.
+ */
+fun main() {
+
+}
